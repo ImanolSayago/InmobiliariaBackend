3ae1b1c5e9c57defd9804ec3e5fe4b93796b760e
ImanolSayago <imanolsayago0@gmail.com>
2025-04-15 21:51:40 -0300

    Backend Inmobiliaria

--- src/main/java/com/InmobiliariaSayago/InmobiliariaSayago/Controller/ControllerPropiedad.java
+++ src/main/java/com/InmobiliariaSayago/InmobiliariaSayago/Controller/ControllerPropiedad.java
@@ -6,6 +6,7 @@ import com.InmobiliariaSayago.InmobiliariaSayago.Respuestas.ResponseMessage;
 import com.InmobiliariaSayago.InmobiliariaSayago.Service.ServicePropiedad;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.OneToMany;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
